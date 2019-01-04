69ac9908cb3b2e973775f37c1fa6ee32e0d98b3f
rainbow <1436839495@qq.com>
2019-01-04 19:14:07 +0800

    加上前端的功能

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.db import models
-from .models import BlogArticles
-
+from blog.models import BlogArticles
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+from django.core import serializers
+import json
 # Create your views here.
 
-p1 = BlogArticles()
+
+def blog_title(request):
+	response = {}
+	blog_list = []
+	print(request.method)
+	data = json.loads(bytes.decode(request.body))
+
+	print(data)
+	user = User.objects.get(username=data['username'])
+	print(user.username)
+	blogs = BlogArticles.objects.all()
+	for blog in blogs:
+		blog_list.append(blog.title)
+	print(blog_list)
+	response['blog'] = [blog.title for blog in blogs]
+	response['msg'] = 'success'
+	return JsonResponse(response)
+
+
